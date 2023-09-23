664965e2f6816f20dbe5d6e438a8210f6fc36712
Letícia Brasileiro <CS374170@MinhaTI.com.br>
2023-09-23 16:45:17 -0300

    criação de menu

--- MoviesApp/MoviesApp/Program.cs
+++ MoviesApp/MoviesApp/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("### MoviesApp ###");
+            Console.WriteLine("1 - Cadastrar nome do usuário");
+            Console.WriteLine("2 - Cadastrar gênero de filme");
+            Console.WriteLine("3 - Cadastrar filme");
+            Console.WriteLine("4 - Listar filmes cadastrados");
+            Console.WriteLine("5 - Sair");
+
+            Console.Write("\nDigite o número da opção desejada: ");
+            Console.ReadLine();
         }
     }
 }

dbc49da9fb27ac418e186b757685f83f9ab82f76
Letícia Brasileiro <CS374170@MinhaTI.com.br>
2023-09-23 21:50:32 -0300

    salva nome do usuário

--- MoviesApp/MoviesApp/Program.cs
+++ MoviesApp/MoviesApp/Program.cs
@@ -2,7 +2,27 @@
 {
     internal class Program
     {
+        static string option; // tipo string para evitar exception/erro de conversão
+        static string userName = "";
+
         static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            while (!endApp)
+            {
+                ShowMenu();
+
+                if (option == "5")
+                {
+                    endApp = true;
+                }
+            }
+
+            Console.WriteLine($"Até logo {userName}!");
+        }
+
+        private static void ShowMenu()
         {
             Console.WriteLine("1 - Cadastrar nome do usuário");
             Console.WriteLine("2 - Cadastrar gênero de filme");
@@ -11,7 +31,36 @@
             Console.WriteLine("5 - Sair");
 
             Console.Write("\nDigite o número da opção desejada: ");
-            Console.ReadLine();
+            option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    Console.Write("\nNome do usuário: ");
+                    userName = Console.ReadLine();
+                    Console.WriteLine("");
+                    break;
+
+                case "2":
+                    Console.WriteLine("\n* Em desenvolvimento *\n");
+                    break;
+
+                case "3":
+                    Console.WriteLine("\n* Em desenvolvimento *\n");
+                    break;
+
+                case "4":
+                    Console.WriteLine("\n* Em desenvolvimento *\n");
+                    break;
+
+                case "5":
+                    Console.WriteLine("\nSaindo...\n");
+                    break;
+
+                default:
+                    Console.WriteLine("\nOpção inválida!\n");
+                    break;
+            }
         }
     }
 }
